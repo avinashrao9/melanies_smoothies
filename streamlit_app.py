00efdcd57c9f824e68d3ca3e01283ab1bc4eff70
avinashrao9 <avinashrao9@gmail.com>
2026-04-01 00:45:43 +0200

    Fix URL formatting in requests.get call

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,5 +53,5 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!,' + name, icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
